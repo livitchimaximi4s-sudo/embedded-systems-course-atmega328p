9b9d6e47ae8b5bd4c0546cb68a2cb133e4aeacba
livitchimaximi4s-sudo <livitchi.maxim.i4s@student.ucv.ro>
2026-05-13 00:57:59 +0300

    sincronizare semafoare

--- src/main.c
+++ src/main.c
@@ -1,81 +1,128 @@
-#include <stdint.h>
 #include "drivers/gpio/gpio.h"
 #include "drivers/timer/timer0.h"
 
-void delay_ms(uint32_t ms)
+#define GREEN_TIME   5000
+#define YELLOW_TIME  2000
+#define EXTRA_TIME   4000
+
+void wait_ms(uint32_t ms)
 {
     uint32_t start = Millis();
+    while ((Millis() - start) < ms);
+}
 
-    while (Millis() - start < ms)
-    {
-    }
+void all_red(void)
+{
+    // Petre Ispirescu
+    GPIO_Write(GPIO_PORTD, 2, GPIO_HIGH);
+    GPIO_Write(GPIO_PORTD, 3, GPIO_LOW);
+    GPIO_Write(GPIO_PORTD, 4, GPIO_LOW);
+
+    // Strada Spania
+    GPIO_Write(GPIO_PORTD, 5, GPIO_HIGH);
+    GPIO_Write(GPIO_PORTD, 6, GPIO_LOW);
+    GPIO_Write(GPIO_PORTD, 7, GPIO_LOW);
+
+    // Calea Bucuresti stanga
+    GPIO_Write(GPIO_PORTB, 0, GPIO_HIGH);
+    GPIO_Write(GPIO_PORTB, 1, GPIO_LOW);
+    GPIO_Write(GPIO_PORTB, 2, GPIO_LOW);
+
+    // Calea Bucuresti dreapta
+    GPIO_Write(GPIO_PORTB, 3, GPIO_HIGH);
+    GPIO_Write(GPIO_PORTB, 4, GPIO_LOW);
+    GPIO_Write(GPIO_PORTB, 5, GPIO_LOW);
+
+    // Viraj spre Spania
+    GPIO_Write(GPIO_PORTC, 0, GPIO_HIGH);
+    GPIO_Write(GPIO_PORTC, 1, GPIO_LOW);
+    GPIO_Write(GPIO_PORTC, 2, GPIO_LOW);
 }
 
 int main(void)
 {
     Timer0_Init();
 
-    GPIO_Init(GPIO_PORTD, 2, GPIO_OUTPUT); // rosu D2
-    GPIO_Init(GPIO_PORTD, 3, GPIO_OUTPUT); // galben D3
-    GPIO_Init(GPIO_PORTD, 4, GPIO_OUTPUT); // verde D4
+    // Petre Ispirescu
+    GPIO_Init(GPIO_PORTD, 2, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTD, 3, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTD, 4, GPIO_OUTPUT);
+
+    // Strada Spania
+    GPIO_Init(GPIO_PORTD, 5, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTD, 6, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTD, 7, GPIO_OUTPUT);
+
+    // Calea Bucuresti stanga
+    GPIO_Init(GPIO_PORTB, 0, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTB, 1, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTB, 2, GPIO_OUTPUT);
+
+    // Calea Bucuresti dreapta
+    GPIO_Init(GPIO_PORTB, 3, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTB, 4, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTB, 5, GPIO_OUTPUT);
+
+    // Viraj spre Spania
+    GPIO_Init(GPIO_PORTC, 0, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTC, 1, GPIO_OUTPUT);
+    GPIO_Init(GPIO_PORTC, 2, GPIO_OUTPUT);
 
     while (1)
     {
-        GPIO_Write(GPIO_PORTD, 2, GPIO_HIGH);
-        GPIO_Write(GPIO_PORTD, 3, GPIO_LOW);
-        GPIO_Write(GPIO_PORTD, 4, GPIO_LOW);
-        delay_ms(2000);
+        // faza 1: Petre + Spania verde
+        all_red();
 
         GPIO_Write(GPIO_PORTD, 2, GPIO_LOW);
-        GPIO_Write(GPIO_PORTD, 3, GPIO_HIGH);
-        GPIO_Write(GPIO_PORTD, 4, GPIO_LOW);
-        delay_ms(1000);
-
-        GPIO_Write(GPIO_PORTD, 2, GPIO_LOW);
-        GPIO_Write(GPIO_PORTD, 3, GPIO_LOW);
         GPIO_Write(GPIO_PORTD, 4, GPIO_HIGH);
-        delay_ms(2000);
-    }
 
-    return 0;
-}#include <stdint.h>
-#include "drivers/gpio/gpio.h"
-#include "drivers/timer/timer0.h"
+        GPIO_Write(GPIO_PORTD, 5, GPIO_LOW);
+        GPIO_Write(GPIO_PORTD, 7, GPIO_HIGH);
 
-void delay_ms(uint32_t ms)
-{
-    uint32_t start = Millis();
+        wait_ms(GREEN_TIME);
 
-    while (Millis() - start < ms)
-    {
-    }
-}
-
-int main(void)
-{
-    Timer0_Init();
-
-    GPIO_Init(GPIO_PORTD, 2, GPIO_OUTPUT); // rosu D2
-    GPIO_Init(GPIO_PORTD, 3, GPIO_OUTPUT); // galben D3
-    GPIO_Init(GPIO_PORTD, 4, GPIO_OUTPUT); // verde D4
-
-    while (1)
-    {
-        GPIO_Write(GPIO_PORTD, 2, GPIO_HIGH);
-        GPIO_Write(GPIO_PORTD, 3, GPIO_LOW);
+        // galben
         GPIO_Write(GPIO_PORTD, 4, GPIO_LOW);
-        delay_ms(2000);
-
-        GPIO_Write(GPIO_PORTD, 2, GPIO_LOW);
         GPIO_Write(GPIO_PORTD, 3, GPIO_HIGH);
-        GPIO_Write(GPIO_PORTD, 4, GPIO_LOW);
-        delay_ms(1000);
 
-        GPIO_Write(GPIO_PORTD, 2, GPIO_LOW);
-        GPIO_Write(GPIO_PORTD, 3, GPIO_LOW);
-        GPIO_Write(GPIO_PORTD, 4, GPIO_HIGH);
-        delay_ms(2000);
+        GPIO_Write(GPIO_PORTD, 7, GPIO_LOW);
+        GPIO_Write(GPIO_PORTD, 6, GPIO_HIGH);
+
+        wait_ms(YELLOW_TIME);
+
+        // faza 2: Bucuresti stanga + dreapta verde
+        all_red();
+
+        GPIO_Write(GPIO_PORTB, 0, GPIO_LOW);
+        GPIO_Write(GPIO_PORTB, 2, GPIO_HIGH);
+
+        GPIO_Write(GPIO_PORTB, 3, GPIO_LOW);
+        GPIO_Write(GPIO_PORTB, 5, GPIO_HIGH);
+
+        wait_ms(GREEN_TIME);
+
+        // faza 3: Bucuresti stanga rosu, dreapta continua
+        GPIO_Write(GPIO_PORTB, 2, GPIO_LOW);
+        GPIO_Write(GPIO_PORTB, 1, GPIO_HIGH);
+
+        wait_ms(YELLOW_TIME);
+
+        GPIO_Write(GPIO_PORTB, 1, GPIO_LOW);
+        GPIO_Write(GPIO_PORTB, 0, GPIO_HIGH);
+
+        // semafor viraj spre Spania
+        GPIO_Write(GPIO_PORTC, 0, GPIO_LOW);
+        GPIO_Write(GPIO_PORTC, 2, GPIO_HIGH);
+
+        wait_ms(EXTRA_TIME);
+
+        // galben final
+        GPIO_Write(GPIO_PORTB, 5, GPIO_LOW);
+        GPIO_Write(GPIO_PORTB, 4, GPIO_HIGH);
+
+        GPIO_Write(GPIO_PORTC, 2, GPIO_LOW);
+        GPIO_Write(GPIO_PORTC, 1, GPIO_HIGH);
+
+        wait_ms(YELLOW_TIME);
     }
-
-    return 0;
 }
